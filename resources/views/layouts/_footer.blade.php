f78423d09ba4bceb1129ee66dbea74bf4043cc27
vagrant <zhy6526@126.com>
2018-04-18 06:19:56 +0000

    Finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="https://weibo.com/u/3492518923/home?leftnav=1" target="_blank">Fifth</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="zhy6526@126.com">联系我</a></p>
+    </div>
+</footer>
---
+++ resources/views/pages/root.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('title', '首页')
+
+@section('content')
+  <h1>这里是工作票管理系统首页</h1>
+@stop
