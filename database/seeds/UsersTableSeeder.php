57c6f0c531e2c45c57f549da2a047ffd9bb99146
vagrant <zhy6526@126.com>
2018-04-22 05:27:44 +0000

    Seeding topics and users data

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,71 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        // 获取 Faker 实例
+        $faker = app(Faker\Generator::class);
+
+        // 头像假数据
+        $avatars = [
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/s5ehp11z6s.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/Lhd1SHqu86.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/LOnMrqbHJn.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/xAuDMxteQy.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/NDnzMutoxX.png?imageView2/1/w/200/h/200',
+        ];
+
+        // 生成数据集合
+        $users = factory(User::class)
+                        ->times(10)
+                        ->make()
+                        ->each(function ($user, $index)
+                            use ($faker, $avatars)
+        {
+            // 从头像数组中随机取出一个并赋值
+            $user->avatar = $faker->randomElement($avatars);
+        });
+
+        // 让隐藏字段可见，并将数据集合转换为数组
+        $user_array = $users->makeVisible(['password', 'remember_token'])->toArray();
+
+        // 插入到数据库中
+        User::insert($user_array);
+
+        // 单独处理第一个用户的数据
+        $user = User::find(1);
+        $user->name = 'Fifth';
+        $user->email = 'Fifth@electric.com';
+        $user->password = bcrypt('123456');
+        $user->admin = 1;
+        $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200';
+        $user->save();
+        $user = User::find(2);
+        $user->name = '系统集成部管理员';
+        $user->email = 'jichengbu@electric.com';
+        $user->password = bcrypt('123456');
+        $user->admin = 2;
+        $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200';
+        $user->save();
+        $user = User::find(3);
+        $user->name = '安保部管理员';
+        $user->email = 'anbao@electric.com';
+        $user->password = bcrypt('123456');
+        $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200';
+        $user->admin = 3;
+        $user->save();
+        $user = User::find(4);
+        $user->name = '运维部管理员';
+        $user->email = 'yunwei@electric.com';
+        $user->password = bcrypt('123456');
+        $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200';
+        $user->admin = 4;
+        $user->save();
+
+    }
+}
